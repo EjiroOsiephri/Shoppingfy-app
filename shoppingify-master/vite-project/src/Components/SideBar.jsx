0a3de9c2c2dc11dfc9b4fe13d7b20370f624fb66
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-02 11:11:16 +0100

    Done with navBar styling

---
+++ shoppingify-master/vite-project/src/Components/SideBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Classes from "../Sass/SideBar.module.scss";
+import appLogo from "../assets/logo.svg";
+import poll from "../assets/poll.svg";
+import rotate from "../assets/rotate.svg";
+import lists from "../assets/list.svg";
+import Cart from "../assets/cart.png";
+
+const SideBar = () => {
+  return (
+    <>
+      <nav className={Classes.navigation}>
+        <div className={Classes.logoDiv}>
+          <img src={appLogo} alt="" />
+        </div>
+        <div className={Classes.navigatePages}>
+          <img src={poll} alt="" />
+          <img src={rotate} alt="" />
+          <img src={lists} alt="" />
+        </div>
+        <div className={Classes.cartItems}>
+          <img src={Cart} alt="cart" />
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default SideBar;
--- shoppingify-master/vite-project/src/main.jsx
+++ shoppingify-master/vite-project/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Provider from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import store from "./Store/ReduxStore.jsx";
 import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
