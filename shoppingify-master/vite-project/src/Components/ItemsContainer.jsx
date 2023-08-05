38ce0da0385aec079279bdbbb2da40f9fbf0fb10
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-05 09:24:59 +0100

    Styling the cart section

--- shoppingify-master/vite-project/src/Components/ItemsContainer.jsx
+++ shoppingify-master/vite-project/src/Components/ItemsContainer.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Classes from "../Sass/ItemsContainer.module.scss";
+import Source from "../assets/source.svg";
+import EmptyCart from "../assets/shop.svg";
 
 const ItemsContainer = () => {
   return (
     <>
-      <div className={Classes["items-container"]}></div>
+      <aside className={Classes["items-container"]}>
+        <section className={Classes["AddItemToCartContainer"]}>
+          <img src={Source} alt="img-source" />
+          <div>
+            <p>Didn’t find what you need?</p>
+            <button>Add item</button>
+          </div>
+        </section>
+        <div className={Classes["showItemsInCart"]}>
+          <p>No items</p>
+          <img src={EmptyCart} alt="" />
+        </div>
+        <div className={Classes["enterFoodItem"]}>
+          <input type="text" placeholder="Enter a name" />
+          <button>Save</button>
+        </div>
+      </aside>
     </>
   );
 };
