13b8de2b61f2050e18018768c0faad3122752c2d
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-16 20:53:57 +0100

    Done with categories and item data

--- shoppingify-master/vite-project/src/pages/Progress.jsx
+++ shoppingify-master/vite-project/src/pages/Progress.jsx
@@ -9,6 +9,23 @@ const Progress = () => {
     (a, b) => b.quantity - a.quantity
   );
 
+  const titleCounts = {};
+  statisticsState.forEach((item) => {
+    const title = item.title;
+    if (titleCounts[title]) {
+      titleCounts[title]++;
+    } else {
+      titleCounts[title] = 1;
+    }
+  });
+
+  const sortedCategories = Object.keys(titleCounts)
+    .map((title) => ({
+      title,
+      count: titleCounts[title],
+    }))
+    .sort((a, b) => b.count - a.count);
+
   return (
     <>
       <main className={Classes["progress-main"]}>
@@ -19,17 +36,35 @@ const Progress = () => {
               <aside key={index} className={Classes["progress-container"]}>
                 <div className={Classes["progress-label"]}>
                   <p>{item.name}</p>
-                  <p>{100}</p>
+                  <p>100%</p>
                 </div>
                 <section className={Classes["progress-bar"]}>
-                  <div className={Classes["progress-bar-fill"]}></div>
+                  <div
+                    className={Classes["progress-bar-fill"]}
+                    style={{ width: `${item.quantity}` }}
+                  ></div>
                 </section>
               </aside>
             );
           })}
         </section>
-        <section className="topCategories">
+        <section className={Classes["topCategories"]}>
           <h1>Top Categories</h1>
+          {sortedCategories.slice(0, 3).map((item, index) => {
+            return (
+              <aside key={index} className={Classes["progress-container"]}>
+                <div className={Classes["progress-label"]}>
+                  <p>{item.title}</p>
+                  <p>100%</p>
+                </div>
+                <section className={Classes["progress-bar"]}>
+                  <div
+                    className={Classes["progress-bar-fill-categories"]}
+                  ></div>
+                </section>
+              </aside>
+            );
+          })}
         </section>
       </main>
     </>
